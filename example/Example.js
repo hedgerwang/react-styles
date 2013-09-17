254e5d339c5186e62d7e527248074fd5c1001704
hedgerwang <hedger@hedger-mbp.local>
2013-09-17 11:45:12 -0700

    Update example

--- example/Example.js
+++ example/Example.js
@@ -14,23 +14,39 @@
  * @jsx React.DOM
  */
 
+// Require pre-defined style rules.
 var ExampleStyleRules = require('ExampleStyleRules');
 var React = require('React');
 var ReactStyle = require('ReactStyle');
 
-ReactStyle.addRules(ExampleStyleRules);
+// Define your own style rules.
+var AnotherStyleRules = ReactStyle.create({
+  '.body': {
+    borderWidth: '10px'
+  }
+});
 
+// Add style rules to site.
+ReactStyle.addRules(ExampleStyleRules);
+ReactStyle.addRules(MoreStyleRules);
+
+// Cache references of classNames for later use.
 var ExampleClassName = ExampleStyleRules.ClassName;
+var AnotherStyleRulesClassName = AnotherStyleRules.ClassName;
 
 var Example = React.createClass({
   render: function() {
+    var className = [
+      ExampleClassName.body,
+      AnotherStyleRulesClassName.body
+    ].join(' ');
     return (
       <html>
         <head>
           <title>React Styles Example</title>
           {ReactStyle.renderToComponents()}
         </head>
-        <body class={ExampleClassName.body}>
+        <body class={className}>
           Hello World
         </body>
       </html>
