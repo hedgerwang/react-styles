77b0d8e989ec7964f59b9e9ade9d9bc3b3f1c2fa
hedgerwang <hedger@hedger-mbp.local>
2013-09-19 20:37:29 -0700

    Add <ReactStyleHead />

---
+++ src/ReactStyleHead.js
@@ -0,0 +1,33 @@
+/**
+ * @providesModule ReactStyleHead
+ */
+
+'use strict'
+
+var React = require('React');
+var ReactStyle = require('ReactStyle');
+
+var ReactStyleHead = React.createClass({
+  componentDidMount: function() {
+    ReactStyle.addEventListener('change', this.handleStyleChange);
+  },
+
+  componentWillUnmount: function() {
+    ReactStyle.removeEventListener('change', this.handleStyleChange);
+  },
+
+  handleStyleChange: function() {
+    this.forceUpdate();
+  },
+
+  render: function() {
+    return this.transferPropsTo(
+      <head>
+        {this.props.children}
+        {ReactStyle.renderToComponents()}
+      </head>);
+    );
+  }
+});
+
+module.exports = ReactStyleHead;
